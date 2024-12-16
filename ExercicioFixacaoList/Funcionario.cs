6fbcce9bbf5a2e14ab6b506c44d85198181628a3
Gabriela <gabriela.priaro@gmail.com>
2024-12-16 16:47:37 +0000

    Add project files.

---
+++ ExercicioFixacaoList/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacaoList
+{
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AdicionarAumento(double porcentagem) 
+        { 
+            double aumento = Salario * porcentagem / 100;
+            Salario = Salario + aumento;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ExercicioFixacaoList/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacaoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> listaFuncionarios = new List<Funcionario>();
+
+            Console.Write("Quantos funcionarios serao registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nFuncionario #{i + 1}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                listaFuncionarios.Add(new Funcionario(id, nome, salario));
+            }
+
+            Console.Write("\nDigite o ID do funcionario que deseja aumentar o salario: ");
+            int idAumentoSalario = int.Parse(Console.ReadLine());
+
+            Funcionario funcionarioAumento = listaFuncionarios.Find(f => f.Id == idAumentoSalario);
+
+            if (funcionarioAumento == null)
+            {
+                Console.WriteLine("Id digitado nao existe");
+            }
+            else
+            {
+                Console.Write("Digite a porcentagem: ");
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                funcionarioAumento.AdicionarAumento(porcentagem);
+            }
+
+            Console.WriteLine("\nLista atualizada de funcionarios:");
+            ImprimirListaFuncionarios(listaFuncionarios);
+        }
+
+        private static void ImprimirListaFuncionarios(List<Funcionario> funcionarios)
+        {
+            foreach (Funcionario funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario.ToString());
+            }
+        }
+    }
+}
